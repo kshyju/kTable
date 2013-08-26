c074882a5114bd961232602d6f2fb0ec295d6d63
Shyju <connectshyju@gmail.com>
2013-08-26 14:33:13 -0400

    Added support for jQuery ui date picker
    Added support for jQuery ui date picker on date filters
    User can define what options should appear in the dropdown

--- ktable.js
+++ ktable.js
@@ -10,21 +10,24 @@
     };
     $.fn.ktable = function (options) {
         //default options
-        var settings = $.extend({
-                                    headerCheckBoxClass: "chkAll",
-                                    checkedRowClass: "rowSelected",
-                                    singleRowCheckBoxClass: "chkSelect",
-                                    rowHighlightClass: "rowHover",
-                                    headerClass: "tHeader",
-                                    filterable: {
-                                        filterableColumnClassName: 'filterable',
-                                        callBackOnFilter: '',
-                                        callBackOnClear: ''
-                                    }
-        }, options);
+        var settings = {
+            headerCheckBoxClass: "chkAll",
+            checkedRowClass: "rowSelected",
+            singleRowCheckBoxClass: "chkSelect",
+            rowHighlightClass: "rowHover",
+            headerClass: "tHeader",
+            filterable: {
+                filterHeaderText: "Show me items with value:",
+                filterAndText: "And",
+                filterableColumnClassName: 'filterable',
+                callBackOnFilter: '',
+                callBackOnClear: '',
+                bindDefaultDatePicker: false,
+                dateFormat: "mm/dd/yy"
+            }
+        };
 
-
-
+        $.extend(true,settings,options);      
 
 
        return this.each(function () {
@@ -76,22 +79,19 @@
 
                var anchorId = headerText.split('/').join('').split(' ').join('-');
 
-
                // console.log(anchorId);
-               var newHeaderMarkup = "<a href='#' datatype='" + dataType + "' id=" + anchorId + " class='k-grid-filter'><span class='k-filter'></span></a><a class='sortHeader'>" + headerText + "</a>";
+               var newHeaderMarkup = "<a href='#' datatype='" + dataType + "' id=" + anchorId + " class='ktable-filter'><span class='k-filter'></span></a><a class='sortHeader'>" + headerText + "</a>";
                _header.html(newHeaderMarkup);
 
            });
 
            //event binding for the dynamically added elements
-           $(document).on("click", "a.k-grid-filter", function (e) {
+           $(document).on("click", "a.ktable-filter", function (e) {           
 
                //build the animation container
                var _clickedAnchor = $(this);
                clickedAnchorId = _clickedAnchor.attr("id");
-               var popupId = "popup-" + clickedAnchorId;
-
-
+               var popupId = "popup-" + clickedAnchorId;              
 
                //Hide existing open popups, remove the selected filter css class
                $(".popupContainer").slideUp(200);
@@ -105,20 +105,19 @@
 
                var headerHeight = _clickedAnchor.closest("tr").height();
                var top = _clickedAnchor.closest("tr").offset().top + headerHeight;
-               var left = e.pageX - _clickedAnchor.closest(".k-grid-filter").width();
+               var left = e.pageX - _clickedAnchor.closest(".k-grid-filter").width();             
 
-
-
-               //console.log(top + " ," + left + ",headerHeight:" + headerHeight);
-               var popupMarkup = "<div id='" + popupId + "' for='" + clickedAnchorId + "' class='popupContainer' style='display:none;position:absolute;top:" + top + "px; left:" + left + "px'>";
+               
+               var popupMarkup = "<div id='" + popupId + "' for='" + clickedAnchorId + "' class='popupContainer' style='display:none;position:absolute;top:" + top + "px; left:" + left + "px;'>";
                popupMarkup += "<form><div>";
-               popupMarkup += "<div class='filterHeader'>Show items with value :</div>";
+               popupMarkup += "<div class='filterHeader'>" + settings.filterable.filterHeaderText + "</div>";
+               popupMarkup += "<div class='searchType'><select class='ddlSearchType'>" + getSelectOptions (_clickedAnchor)+ "</select></div>";
                if (_clickedAnchor.attr("datatype") === "date") {
-                   popupMarkup += "<div class='searchType'><select class='ddlSearchType'><option>Equal to</option><option>Before</option><option>After</option></select></div>";
-                   popupMarkup += "<div class='searchBox'><input type='text' class='filter-input datePicker' /></div>";
+                 
+                   popupMarkup += "<div class='searchBox'><input type='text' class='filter-input kTableDatePicker' /></div>";
+                   popupMarkup += "<div class='divBetween' style='display:none;'>" + settings.filterable.filterAndText + "<br/><div class='searchBox'><input type='text' class='filter-input kTableDatePicker' /></div></div>";
                }
-               else {
-                   popupMarkup += "<div class='searchType'><select class='ddlSearchType'><option>Contains</option><option>Starts with</option><option>Ends with</option></select></div>";
+               else {                 
                    popupMarkup += "<div class='searchBox'><input type='text' class='filter-input' /></div>";
                }
 
@@ -131,10 +130,21 @@
                //highlight the clicked filter icon
                _clickedAnchor.find("span.k-filter").addClass("filter-selected");
 
+               //Bind date picker to the ui
+               
+               if (settings.filterable.bindDefaultDatePicker) {                               
+                   if (jQuery.ui) {
+                       {
+                           $("input.kTableDatePicker").datepicker({ dateFormat: settings.filterable.dateFormat });
+                       }
+                   }
+               }
+
            });
 
            //Execute the callback function when clicking on the filter button 
            $(document).on("click", "button[type='submit']", function (e) {
+               e.preventDefault();
                var _clickedSubmit = $(this);
                if (settings.filterable.callBackOnFilter)
                    settings.filterable.callBackOnFilter(_clickedSubmit.closest("form"));
@@ -144,6 +154,7 @@
 
            //Close the popup when clicking
            $(document).on("click", "button[type='reset']", function (e) {
+               e.preventDefault();
                var _clickedReset = $(this);
                var anchorId = _clickedReset.closest("div.popupContainer").attr("for");
                _clickedReset.closest("div.popupContainer").hide();
@@ -153,19 +164,52 @@
                    settings.filterable.callBackOnClear(_clickedReset.closest("form"));
            });
 
+              //Change event on the ddlSearchType SELECT 
+              //If user selected between option, show the end date textbox too;
+           $(document).on("click", "SELECT.ddlSearchType", function (e) {
+               var val = $(this).val();
+               if (val == "between")
+                   $("div.divBetween").fadeIn(100);
+               else
+                   $("div.divBetween").fadeOut(100).find("input").val("");
+           });
+
+
+
           } //if filter is enabled
 
        }); // return this.each
 
      
-
+       function getSelectOptions(_clickedAnchor) {           
+           if (typeof settings.filterable.dropDownContent !== 'undefined') {            
+               var dropDownID = "";               
+               if (_clickedAnchor.attr("datatype") === "date") {
+                   dropDownID = settings.filterable.dropDownContent.source.date;
+               }
+               else {
+                   dropDownID = settings.filterable.dropDownContent.source.text;
+               }
+              
+               var strOptions = "";
+               $("#" + dropDownID + " option").each(function () {                  
+                   strOptions += "<option value='" + $(this).val() + "'>" + $(this).text() + "</option>";
+               });
+               return strOptions;
+           }
+           else {
+               if (_clickedAnchor.attr("datatype") !== "date") {
+                   return "<option>Contains</option><option>Starts with</option><option>Ends with</option>"
+               }
+               else {
+                   return "<option>Equal to</option><option>Before</option><option>After</option>";
+               }
+           }
+        };
 
 
     };      // plugin ends
 
 })(jQuery, window, document);
 
-//event binding for the dynamically added elements
-
-//event binding for the dynamically added elements
 
